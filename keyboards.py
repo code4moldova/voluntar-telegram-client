8a99e5536fea08fd78eb729b7dcb23c491424ba1
Alex Railean @xlippery <a.railean@dekart.com>
2020-04-06 21:22:40 +0200

    Reconcile black and flake8 (black wins)

--- keyboards.py
+++ keyboards.py
@@ -164,10 +164,7 @@ def get_etas_today(time_from=None):
 def chunkify(lst, n=4):
     """Yield successive n-sized chunks from lst. Taken from https://stackoverflow.com/a/312464/27342"""
     for i in range(0, len(lst), n):
-        yield lst[
-            i : i
-            + n  # noqa: E203  # black makes it ugly, conflicts with flake8, you can't make 'em both happy
-        ]
+        yield lst[i:i+n]
 
 
 def build_dynamic_keyboard(time_from=None):
